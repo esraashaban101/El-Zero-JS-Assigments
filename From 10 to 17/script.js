df87b081278328b38b0c741983d3861edadf0844
esraashaban101 <es915419@gmail.com>
2024-05-16 19:30:01 +0300

    js assigments

---
+++ From 10 to 17/script.js
@@ -0,0 +1,26 @@
+//Q1
+var numberOne = 10,
+  numberTwo = 20;
+let res = numberOne + "" + numberTwo;
+console.log(res);
+console.log(typeof res);
+let res2 = `${numberOne}${numberTwo}`;
+console.log(res2);
+console.log(typeof res2);
+console.log(numberTwo + "\n" + numberOne);
+console.log(`${numberTwo} 
+${numberOne}`)
+
+//Q2
+// console.log(elzero.innerHTML)
+
+console.log(typeof elzero);
+
+//Q3
+console.log("`I'm In \n Love \\\\ \"\"\"\ \'\'\'\"\ ++ with ++ \n \\ \"\"\" \\ \"\"\"  \n \"\"\"JavaScript\"\"\`\`")
+
+
+//Q4
+let a= 21,b=20,c="_2021_2021_2021_";
+
+console.log(`_ ${a}${c}${b}_`)
